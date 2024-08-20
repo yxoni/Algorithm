e88f8c21e91168afa1c6c4f71835d5ee6903e6cd
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2024-08-20 15:58:59 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        
+        int day = 1;
+        if (v-a > 0) {
+            if ((v-a)/(a-b) == 0) {
+                day++;
+            } else if ((v-a)%(a-b) == 0) {
+                day += (v-a)/(a-b);      
+            } else {
+                day += ((v-a)/(a-b) + 1);
+            }
+        }  
+   
+        bw.write(String.valueOf(day));
+        bw.flush();
+        bw.close();
+    }
+}
