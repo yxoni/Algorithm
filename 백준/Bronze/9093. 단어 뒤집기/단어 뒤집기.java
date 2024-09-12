c710f0a04365d0a7b777092eec9c953409f0cd1c
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2024-09-12 21:03:53 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 416 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,26 +1,19 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
 
-        List<String> list;
+        String str;
         for(int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
-            list = new ArrayList<>();
+            str = br.readLine();
 
-            while(st.hasMoreTokens()) {
-                list.add(st.nextToken());
-            }
-
-            for(int j = 0; j < list.size(); j++) {
-                for (int k = list.get(j).length() - 1; k >= 0; k--) {
-                    bw.write(list.get(j).charAt(k));
+            for (String s : str.split(" ")) {
+                for(int j = s.length()-1; j >= 0; j--) {
+                    bw.write(s.charAt(j));
                 }
                 bw.write(" ");
             }
@@ -28,6 +21,5 @@ public class Main {
         }
         bw.flush();
         bw.close();
-
     }
 }
