00bab71c42ac05f80dab7e3f3d4a658fb56a93fe
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2024-09-24 20:12:52 +0900

    [Bronze I] Title: 소인수분해, Time: 228 ms, Memory: 26744 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -6,7 +6,7 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        boolean[] arr = new boolean[10000001];
+        boolean[] arr = new boolean[N+1];
         arr[0] = arr[1] = true;
         for (int i = 2; i < Math.sqrt(arr.length); i++) {
             if (arr[i]) continue;
