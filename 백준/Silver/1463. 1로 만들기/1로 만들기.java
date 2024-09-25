c42c7f0c2ca55b4ecab438539bce2693f90d3f8d
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2024-09-25 21:09:32 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18144 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i-1] +1;
+            if(i%2 == 0) dp[i] = Math.min(dp[i], dp[i/2]+1);
+            if(i%3 == 0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+        }
+        bw.write(String.valueOf(dp[N]));
+        bw.flush();
+        bw.close();
+    }
+}
