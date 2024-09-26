a9c332f329205835061eee9f26f526d24bff6ed3
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2024-09-26 16:09:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        if (n > 1) dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+        bw.close();
+    }
+}
