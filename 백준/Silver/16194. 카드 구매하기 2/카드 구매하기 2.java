bba49da3d16c6c775f1c659f53313a8a1f68b0ba
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2024-09-29 14:13:14 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 120 ms, Memory: 14560 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] dp = new int[N + 1];
+        for (int i = 1; i < N+1; i++) {
+            dp[i] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j < i/2+1; j++) {
+                dp[i] = Math.min(dp[i], dp[j] + dp[i-j]);
+            }
+        }
+        bw.write(String.valueOf(dp[N]));
+        bw.flush();
+        bw.close();
+    }
+}
