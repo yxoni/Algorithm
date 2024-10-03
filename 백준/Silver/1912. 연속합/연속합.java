25484ccd6f24efda2124ef74820cf99068bb7481
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2024-10-03 20:17:19 +0900

    [Silver II] Title: 연속합, Time: 236 ms, Memory: 23624 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[0] = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+    }
+}
