2f095400d1ff2587f856f3a3873aca99c4f280f3
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2024-11-23 01:13:16 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N+1];
+        arr = new int[M];
+        bt(0);
+        System.out.println(sb);
+    }
+
+    public static void bt(int cnt) {
+        if (cnt == M) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (cnt > 0 && arr[cnt-1] > i) continue;
+            arr[cnt] = i;
+            bt(cnt+1);
+        }
+    }
+}
