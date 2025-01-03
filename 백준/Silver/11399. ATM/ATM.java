e34456a1637470c94c8a80b357e34ad1b157a3e7
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2025-01-03 17:51:24 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int sum = 0;
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += arr[i];
+            result += sum;
+        }
+        
+        sb.append(result);
+        System.out.println(result);
+    }
+}
