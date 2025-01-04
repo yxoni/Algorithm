a788ed49c028657675c7cce2752ce67ecef5ff29
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2025-01-04 16:46:12 +0900

    [Silver III] Title: 칸토어 집합, Time: 372 ms, Memory: 51940 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        String str;
+        while ((str = br.readLine()) != null) {
+            int N = Integer.parseInt(str);
+            if (N == 0) {
+                sb.append("-");
+            } else {
+                dq((int) Math.pow(3, N));
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void dq(int n) {
+        int num = n / 3;
+
+        String[] str = new String[num];
+        String[] str1;
+        String[] str2;
+        Arrays.fill(str, " ");
+
+        if (num == 1) {
+            str1 = new String[num];
+            str2 = new String[num];
+            Arrays.fill(str1, "-");
+            Arrays.fill(str2, "-");
+
+            sb.append(String.join("", str1));
+            sb.append(String.join("", str));
+            sb.append(String.join("", str2));
+            return;
+        }
+
+        dq(num);
+        sb.append(String.join("", str));
+        dq(num);
+    }
+}
