fbfc169a48abc55a8381bbab9d10a8d74d4b776b
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2025-01-07 10:54:32 +0900

    [Silver III] Title: N번째 큰 수, Time: 764 ms, Memory: 273924 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                queue.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        
+        for (int i = 0; i < N - 1; i++) {
+            queue.poll();
+        }
+        sb.append(queue.peek());
+        System.out.println(sb);
+    }
+}
