942873bc6afcdbea4bd197047ee24815c32370dc
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2025-02-05 09:04:06 +0900

    [Silver V] Title: 스네이크버드, Time: 112 ms, Memory: 14804 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > L) {
+                break;
+            }
+            L++;
+        }
+
+        sb.append(L);
+        System.out.println(sb);
+    }
+}
