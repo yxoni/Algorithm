97ab90158adeca55455f125f877bcb728ac206c2
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2025-02-12 09:42:46 +0900

    [Silver III] Title: 소수 최소 공배수, Time: 196 ms, Memory: 18488 KB -BaekjoonHub

---
+++ 백준/Silver/21919. 소수 최소 공배수/소수 최소 공배수.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final int SIZE = 2_000_001;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        boolean[] visited = new boolean[SIZE];
+        visited[0] = visited[1] = true;
+        for (int i = 2; i < Math.sqrt(SIZE); i++) {
+            if (!visited[i]) {
+                for (int j = i * i; j < SIZE; j += i) {
+                    if (!visited[j]) {
+                        visited[j] = true;
+                    }
+                }
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (!visited[num]) {
+                list.add(num);
+            }
+        }
+
+        long result = -1L;
+        if (list.size() != 0) {
+            sb = new StringBuilder();
+            Collections.sort(list);
+            result = list.get(0);
+            for (int i = 1; i < list.size(); i++) {
+                int num = list.get(i);
+                if (result > num) {
+                    result = result * num / gcd(result, num);
+                } else {
+                    result = num * result / gcd(num, result);
+                }
+            }
+        }
+
+        sb.append(result);
+        System.out.println(sb);
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
