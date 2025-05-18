dce274e849845ed20a32b07509a1a256688d6bf5
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2025-05-18 12:34:56 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.80 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int ba = Integer.parseInt(String.valueOf(b) + String.valueOf(a));
+
+        return ab >= ba ? ab : ba;
+    }
+}
